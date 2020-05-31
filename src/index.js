dc9a4bb426bc571b2ecb3dba3edbee3e91b16ea2
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2020-05-31 04:51:06 -0400

    Add "redux-logger"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import logger from 'redux-logger'
 
 import rootReducer from './store/reducers'
 
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+)
 
 ReactDOM.render(
   <React.StrictMode>
