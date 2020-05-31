cf8d1d6f87f961604baf85c4cc152bd83b0ae85a
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2020-05-31 01:47:19 -0400

    Add to the App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './store/reducers'
+
 import './index.css';
 import App from './App';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
