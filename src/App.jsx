a5272b8d91649c45230dd4398c9df34eb73a7ee1
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2020-05-31 01:35:09 -0400

    Add actions & reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import Auth from './components/Auth';
+import Registration from './components/Registration';
+
 export default class App extends React.Component {
   render() {
     return (
@@ -8,72 +11,10 @@ export default class App extends React.Component {
         <h1>Complex State</h1>
 
         <div className="forms">
-
-          <div className="auth">
-            <h3>Sign In</h3>
-            <form>
-              <div>
-                <input
-                  type="text"
-                  name="login"
-                  autoComplete="off"
-                  placeholder="E-mail"
-                  spellCheck={false}
-                />
-              </div>
-
-              <div>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
-              </div>
-              
-              <div>
-                <button>Sign In</button>
-              </div>
-              
-            </form>
-            
-          </div>
-
-          <div className="registration">
-            <h3>Sign Up</h3>
-            <form>
-
-              <div>
-                <input
-                  type="text"
-                  name="login"
-                  autoComplete="off"
-                  placeholder="E-mail"
-                  spellCheck={false}
-                />
-              </div>
-
-              <div>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Repeat Password"
-                />
-              </div>
-
-              <div>
-                <button>Sign Up</button>
-              </div>
-
-            </form>
-          </div>
-
+          <Auth />
+          <Registration />
         </div>
-      
+
       </div>
     )
   }
